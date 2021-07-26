73a277208c421d83dc6b41e39191eac9b5be5ee3
大火兔 <dahuotu@users.noreply.github.com>
2021-07-26 22:00:47 +0800

    新增内置API

--- dao/api/dao.js
+++ dao/api/dao.js
@@ -1,7 +1,7 @@
 /*
 说明:实现与MySQL交互
 方式:Api方式
-路径:dao/server/dao.js
+路径:dao/api/dao.js
 */
 
 var $mysql = require('mysql');
@@ -18,6 +18,7 @@ module.exports = {
 		pool.getConnection(function (err, connection) {
 			connection.query($sql.queryTable, function (err2, result) {
 				if (result != null) {
+					console.log(result);
 					result = {
 						code: 0,
 						msg: '查询成功',
@@ -37,8 +38,17 @@ module.exports = {
 	},
 	queryColumn: function (req, res, next) {
 		//查询所有字段,对应路由column
+		var param = req.query;
+		if (param.table == null) {
+			result = {
+				code: -1,
+				msg: '参数错误'
+			};
+			$util.jsonMsg(res, result);
+			return;
+		}
 		pool.getConnection(function (err, connection) {
-			connection.query($sql.queryColumn, function (err2, result) {
+			connection.query($sql.queryColumn, [param.table], function (err2, result) {
 				if (result != null) {
 					result = {
 						code: 0,
